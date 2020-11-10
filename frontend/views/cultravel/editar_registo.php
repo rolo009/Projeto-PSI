5c2cacbc151a63f42a601fe6111fed9f2fedaa4a
Gustavo0123 <2190716@my.ipleiria.pt>
2020-11-10 15:16:42 -0100

    login, registo e editar registo

--- frontend/views/cultravel/editar_registo.php
+++ frontend/views/cultravel/editar_registo.php
@@ -5,13 +5,14 @@ use rmrevin\yii\fontawesome\FA;
 use yii\web\AssetBundle;
 
 ?>
+
 <div class="logo-index-container">
 
     <?= Html::img('@web/seta-logo.png'); ?>
 
-<div class="row">
-    <div class="col-lg-5">
-        <?php $form = ActiveForm::begin()?>
+    <div class="row">
+        <div class="col-lg-5">
+            <?php $form = ActiveForm::begin()?>
 
             <?php
             echo $form->field($modelprofile, 'primeiroNome', ['options' => ['class' => 'label-login']])->textInput(['autofocus' => true]);
@@ -23,12 +24,12 @@ use yii\web\AssetBundle;
             echo $form->field($modelprofile, 'localidade', ['options' => ['class' => 'label-login']]);
             echo $form->field($modelprofile, 'sexo', ['options' => ['class' => 'label-login']])->checkbox();?>
 
-        <div class="form-group">
-        <?php
-        echo Html::submitButton('Registar', ['class' => 'btn btn-warning', 'name' => 'insert-registo']) ?>
-        <?php /* Html::a('<i class="fa fa-fw fa-user"></i> Sign Up',['site/signup'], ['class' => 'btn btn-black', 'title' => 'Sign Up']) */ ?>
-        </div>
-        <?php ActiveForm::end(); ?>
+            <div class="form-group">
+                <?php
+                echo Html::submitButton('Editar Registo', ['class' => 'btn btn-warning', 'name' => 'insert-registo']) ?>
+                <?php /* Html::a('<i class="fa fa-fw fa-user"></i> Sign Up',['site/signup'], ['class' => 'btn btn-black', 'title' => 'Sign Up']) */ ?>
+            </div>
+            <?php ActiveForm::end(); ?>
         </div>
     </div>
 </div>
--- frontend/views/cultravel/login.php
+++ frontend/views/cultravel/login.php
@@ -9,20 +9,32 @@ use yii\web\AssetBundle;
 
     <?= Html::img('@web/seta-logo.png'); ?>
 
-</div>
+    <p class="label-login">Preencha os campos do login:</p>
 
-<div>
-    <div class="searchForm-container">
+<div class="row">
+    <div class="col-lg-5">
         <?php $form = ActiveForm::begin()?>
 
-        <?php
-        echo $form->field($model, 'email', ['options' => ['class' => 'label-login']]);
-        echo $form->field($model, 'password_hash', ['options' => ['class' => 'label-login']]); ?>
+            <?php
+            echo $form->field($model, 'email', ['options' => ['class' => 'label-login']]); ?>
+            <?php
+             echo $form->field($model, 'password_hash', ['options' => ['class' => 'label-login']])->passwordInput(); ?>
 
-        <?php
-        echo Html::submitButton('Iniciar Sessão', ['class' => 'btn btn-warning', 'name' => 'insert-login']) ?>
-        <?php /* Html::a('<i class="fa fa-fw fa-user"></i> Sign Up',['site/signup'], ['class' => 'btn btn-black', 'title' => 'Sign Up']) */ ?>
-        <?php ActiveForm::end(); ?>
+            <div style="color:#999;margin:1em 0">
+                <?= Html::a('Esqueceu-se da palavra-passe?', ['site/request-password-reset']) ?>.
+                <br>
+                <?= Html::a('Verificação do email', ['site/resend-verification-email']) ?>
+            </div>
 
+            <div class="form-group">
+            <?php
+            echo Html::submitButton('Iniciar Sessão', ['class' => 'btn btn-warning', 'name' => 'insert-login']) ?>
+            </div>
+
+            <?php /* Html::a('<i class="fa fa-fw fa-user"></i> Sign Up',['site/signup'], ['class' => 'btn btn-black', 'title' => 'Sign Up']) */ ?>
+
+            <?php ActiveForm::end(); ?>
+        </div>
     </div>
 </div>
+

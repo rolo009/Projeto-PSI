0570234288bc9f6d7a09dfbd9687547d03d99c69
Gustavo0123 <2190716@my.ipleiria.pt>
2020-11-11 13:25:07 -0100

    login, registo e editar registo

--- frontend/views/cultravel/login.php
+++ frontend/views/cultravel/login.php
@@ -5,14 +5,16 @@ use rmrevin\yii\fontawesome\FA;
 use yii\web\AssetBundle;
 
 ?>
-<div class="logo-index-container">
+<div class="login-container">
 
-    <?= Html::img('@web/seta-logo.png'); ?>
+    <?= Html::img('@web/seta-logo.png', ['class' => 'logo-login']); ?>
 
-    <p class="label-login">Preencha os campos do login:</p>
+    <p class="login-info">Preencha os campos do login:</p>
 
 <div class="row">
-    <div class="col-lg-5">
+    <div class="col-xs-1 col-sm-2 col-md-3 col-lg-3">
+    </div>
+    <div class="col-xs-10 col-sm-8 col-md-6 col-lg-6">
         <?php $form = ActiveForm::begin()?>
 
             <?php
@@ -29,12 +31,12 @@ use yii\web\AssetBundle;
             <div class="form-group">
             <?php
             echo Html::submitButton('Iniciar Sessão', ['class' => 'btn btn-warning', 'name' => 'insert-login']) ?>
-            </div>
-
             <?php /* Html::a('<i class="fa fa-fw fa-user"></i> Sign Up',['site/signup'], ['class' => 'btn btn-black', 'title' => 'Sign Up']) */ ?>
-
-            <?php ActiveForm::end(); ?>
-        </div>
+            </div>
+        <?php ActiveForm::end(); ?>
+    </div>
+</div>
+    <div class="col-xs-1 col-sm-3 col-md-2 col-lg-3">
     </div>
 </div>
 

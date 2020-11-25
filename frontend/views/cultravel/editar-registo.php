07c6c4e8584527a9b638ebda42d6e42737012a9b
Gustavo0123 <2190716@my.ipleiria.pt>
2020-11-24 23:11:11 -0100

    login, registo e editar registo

--- frontend/views/cultravel/editar-registo.php
+++ frontend/views/cultravel/editar-registo.php
@@ -21,15 +21,15 @@ use yii\web\AssetBundle;
 
             <?php
             echo $form->field($profile, 'primeiroNome', ['options' => ['class' => 'label-registo']])->textInput(['autofocus' => true]);
-            /*echo $form->field($model, 'ultimoNome', ['options' => ['class' => 'label-registo']])->label("Último Nome");
-            echo $form->field($model, 'username', ['options' => ['class' => 'label-registo']])->label("Nome de Utilizador");
-            echo $form->field($model, 'email', ['options' => ['class' => 'label-registo']])->label("Email");
-            echo $form->field($model, 'dtaNascimento', ['options' => ['class' => 'label-registo']])->label("Data de Nascimento")->widget(\yii\jui\DatePicker::className(), ['dateFormat' => 'yyyy-MM-dd']);
-            echo $form->field($model, 'password', ['options' => ['class' => 'label-registo']])->label("Palavra Passe")->passwordInput();
-            echo $form->field($model, 'confirmPassword', ['options' => ['class' => 'label-registo']])->label("Confirmar Palavra Passe")->passwordInput();
-            echo $form->field($model, 'morada', ['options' => ['class' => 'label-registo']])->label("Morada");
-            echo $form->field($model, 'localidade', ['options' => ['class' => 'label-registo']])->label("Localidade");
-            echo $form->field($model, 'sexo', ['options' => ['class' => 'label-registo']])->radioList( ['Masculino'=>'Masculino', 'Feminino' => 'Feminino'] )->label("Sexo");?>*/
+            echo $form->field($profile, 'ultimoNome', ['options' => ['class' => 'label-registo']])->label("Último Nome");
+            echo $form->field($user, 'username', ['options' => ['class' => 'label-registo']])->label("Nome de Utilizador");
+            echo $form->field($user, 'email', ['options' => ['class' => 'label-registo']])->label("Email");
+            echo $form->field($profile, 'dtaNascimento', ['options' => ['class' => 'label-registo']])->label("Data de Nascimento")->widget(\yii\jui\DatePicker::className(), ['dateFormat' => 'yyyy-MM-dd']);
+            //echo $form->field($user, 'password', ['options' => ['class' => 'label-registo']])->label("Palavra Passe")->passwordInput();
+            //echo $form->field($profile, 'confirmPassword', ['options' => ['class' => 'label-registo']])->label("Confirmar Palavra Passe")->passwordInput();
+            echo $form->field($profile, 'morada', ['options' => ['class' => 'label-registo']])->label("Morada");
+            echo $form->field($profile, 'localidade', ['options' => ['class' => 'label-registo']])->label("Localidade");
+            echo $form->field($profile, 'sexo', ['options' => ['class' => 'label-registo']])->radioList( ['Masculino'=>'Masculino', 'Feminino' => 'Feminino'] )->label("Sexo");?>
 ?>
             <div class="form-group">
                 <?php

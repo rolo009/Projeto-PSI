d05d3729f6202dd3eb3724804c35e3ddb4a81324
Pedro Rolo <pedrorolo@live.com.pt>
2021-01-24 16:49:46 +0000

    FINAL PROJETO

--- frontend/views/cultravel/sobre-nos.php
+++ frontend/views/cultravel/sobre-nos.php
@@ -33,16 +33,16 @@ $this->title = 'Sobre Nós';
 
     <div class="sobrenos-container">
         <div class="sobrenos-card">
-            <?= Html::img('@web/imagens/img-collaborators/pedro.jpeg', ['class' => 'sobrenos-card-img']); ?>
+            <?= Html::img(Yii::$app->urlManagerBackend->baseUrl . '/img-collaborators/pedro.jpeg', ['class' => 'sobrenos-card-img']); ?>
             <div class="sobrenos-card-text">
                 <h3>Pedro Rolo</h3>
                 <p>Elemento da equipa</p>
-
             </div>
         </div>
 
         <div class="sobrenos-card">
-            <?= Html::img('@web/imagens/img-collaborators/gustavo.jpg', ['class' => 'sobrenos-card-img']); ?>
+            <?= Html::img(Yii::$app->urlManagerBackend->baseUrl . '/img-collaborators/gustavo.jpg', ['class' => 'sobrenos-card-img']); ?>
+
             <div class="sobrenos-card-text">
                 <h3>Gustavo Mendonça</h3>
                 <p>Elemento da equipa</p>
@@ -51,7 +51,7 @@ $this->title = 'Sobre Nós';
         </div>
 
         <div class="sobrenos-card">
-            <?= Html::img('@web/imagens/img-collaborators/mario.jpg', ['class' => 'sobrenos-card-img']); ?>
+            <?= Html::img(Yii::$app->urlManagerBackend->baseUrl . '/img-collaborators/mario.jpg', ['class' => 'sobrenos-card-img']); ?>
             <div class="sobrenos-card-text">
                 <h3>Mário Carapinha</h3>
                 <p>Elemento de equipa</p>
